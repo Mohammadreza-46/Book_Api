6bc80c460b94276eea514e6ccbfd22754ee4e763
unknown <ansarimohammadreza46@gmail.com>
2026-06-16 11:30:28 +0330

    bug fix in main.py #9

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ books_bp = books.books_bp
 auth_bp = auth.auth_bp
 jwt_manager = JWTManager(app)
 app.config['JWT_SECRET_KEY'] = '123546578910'
+
+app.register_blueprint(books_bp)
+app.register_blueprint(auth_bp)
 if __name__ == '__main__':
-    app.register_blueprint(books_bp)
-    app.register_blueprint(auth_bp)
     app.run(debug=True, host='0.0.0.0', port=5000)
