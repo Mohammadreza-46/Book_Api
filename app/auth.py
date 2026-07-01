9d60de65567d668ef94710e7f63d7f726fcbdfc2
unknown <ansarimohammadreza46@gmail.com>
2026-07-01 20:21:26 +0330

    edit erorr type in the singup username exists#29

--- app/auth.py
+++ app/auth.py
@@ -48,7 +48,7 @@ def signup():
     data['password'] = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt(12)).decode('utf-8')
     file_name = os.path.join(os.path.join(dir_name,os.path.join('data','Users')),data['username'])+'.json'
     if os.path.exists(file_name):
-        return error_response('username already exists',400)
+        return error_response('username already exists',409)
     with open(file_name, 'w') as f:
         f.write(json.dumps(data))
     return jsonify({'message': 'success'})
